8321a87db2d4fca98db06f1d3688c5f6517d5cf1
JoseAdrianRodriguezGonzalez <kerpador@gmail.com>
2025-06-04 22:57:05 -0600

    Here, I've fixed some rules, some bugs, and optimize some sort of variables

--- src/main/Board.java
+++ src/main/Board.java
@@ -1,49 +1,62 @@
 public class Board{
     //matriz 3x3
-    private Cell[][] cells = new Cell[3][3];
+    private Cell[][] Cells = new Cell[3][3];
 
     //constructor del tablero, inicializa cada celda 
     public Board(){
-        for(int i =0; i < 3; i++){
-            for(int j=0; j < 3; j++){
-                cells[i][j] = new Cell();
+        for(Byte Row =0; Row < 3; Row++){
+            for(byte Column=0; Column < 3; Column++){
+                Cells[Row][Column] = new Cell();
             }
         }
     }
-    
-    public void printBoard() {
-        System.out.println("\n");
-        for (int i = 0; i < 3; i++) { //recorremos las filas
-            for (int j = 0; j < 3; j++) { //ahora las columnas
-                System.out.print(" " + cells[i][j].getValue() +" ");
-                if (j < 2){ // imprime en cada columna,pero en la ultima col no
+    /*La función PrintBoard no recibe ningún párametro, ni devuelve algun valor
+     * Imprime el tablero junto con el valor dentro de la matriz Cells('X'/'O')
+     */
+    public void PrintBoard() {
+        System.out.println();
+        for (byte Rows = 0; Rows < 3; Rows++) { 
+            for (byte Columns = 0; Columns < 3; Columns++) { 
+                System.out.print(" " + Cells[Rows][Columns].GetValue() +" ");
+                if (Columns < 2){ 
                     System.out.print("|");
                 }
             }
             System.out.println();
-            if(i<2){ // imprime en cada columna,pero en la ultima col no
-                System.out.println("___|___|___");
-            }
+            System.out.println(((Rows<2)?"___|___|___":"   |   |   "));
         }
     }
-    //metodo para asignar el valor de cada jugador(X /O)
-    public void SetCell(int row, int col, String value){
-        cells[row][col].setValue(value);
+    /* Requiere la fila y la columna en donde se ingresará el dato,(de 0 a 2) y el valor del string 
+    metodo para asignar el valor de cada jugador(X /O) dentro de la matriz Cells
+    No retorna nada*/
+    public void SetCell(int Row, int Col, String Value){
+        Value=Value.toUpperCase();
+        if(!Value.equals("X")&&!Value.equals("O")){
+            System.out.println("No se permite ese tipo de valor.");
+            return;
+        }
+        Cells[Row][Col].SetValue(Value);
+    }
+    /*Recibe el indice de fila y columna que se desee verficar si está vacía
+     * Retorna un valor booleano
+    */
+    public boolean IsCellEmpty(byte Row,byte Col){
+        return Cells[Row][Col].GetValue().equals(" ");
     }
 }// fin clase Board
 
 
 class Cell{
     //valor de X / O
-    private String value = " ";
+    private String Value = " ";
 
     //recupera value actual de la celda
-    public String getValue(){
-        return value;
+    public String GetValue(){
+        return Value;
     }
     //establece el nuevo value de la celda
-    public void setValue(String aux){
-        value =aux;
+    public void SetValue(String Aux){
+        Value = Aux;
     }
 }// fin clase Cell
 
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,8 @@
 public class Main{
 	public static void main(String [] args){
-		System.out.println("Hello World");
+		Board b =new Board();
+		b.SetCell(1,1,"x");
+		b.PrintBoard();
+		//Ejemplo de uso
 	}
 }
