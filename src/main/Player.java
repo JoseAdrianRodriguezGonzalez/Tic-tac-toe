a0d361afd9e7f620a02c22ff5b1a87d1ebbd1cbc
Adrian <kerpador@gmail.com>
2025-06-06 00:22:16 -0600

    Update Player.java

--- src/main/Player.java
+++ src/main/Player.java
@@ -3,22 +3,22 @@ public abstract class Player {
     * Clase abstracta que representa a un jugador en el juego Tic Tac Toe.
     * Proporciona la estructura base para jugadores humanos e IA.
     */
-    protected char symbol; // 'X' o 'O'
+    protected char Symbol; // 'X' o 'O'
 
     /**
      * Constructor del jugador.
      * @param symbol El símbolo del jugador ('X' o 'O').
      */
-    public Player(char symbol) {
-        this.symbol = Character.toUpperCase(symbol);
+    public Player(char Symbol) {
+        this.Symbol = Character.toUpperCase(symbol);
     }
 
     /**
      * Obtiene el símbolo del jugador.
      * @return El símbolo del jugador.
      */
-    public char getSymbol() {
-        return symbol;
+    public char GetSymbol() {
+        return Symbol;
     }
 
     /**
@@ -28,5 +28,5 @@ public abstract class Player {
      * @param board El tablero actual del juego.
      * @return Un arreglo de dos bytes [fila, columna] indicando la jugada.
      */
-    public abstract byte[] makeMove(Board board);
+    public abstract byte[] MakeMove(Board Board);
 }
