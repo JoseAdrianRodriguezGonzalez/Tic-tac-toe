aa2c79abf0d2682c3eb0d4a5b531ae69cd8b58cf
Rapla913 <92961696+Rapla913@users.noreply.github.com>
2025-06-06 21:42:33 -0600

    Basic Game class structure

---
+++ src/main/Game.java
@@ -0,0 +1,33 @@
+import java.util.Scanner; 
+public class Game {
+    public boolean turn; 
+    private Menu gameMenu; 
+    private Board gameBoard;
+    private Scanner scanner; 
+    private short playerAmount; 
+    /*
+     * El constructor de la clase es el encargado crear los objetos que va a necesitar 
+     * a lo largo del juego. 
+     */
+    public Game() {
+        this.gameBoard = new Board(); 
+        this.gameMenu = new Menu(); 
+        this.scanner = new Scanner(System.in); 
+        this.turn = true;
+        this.playerAmount = 0;  
+    }
+
+    public void play(){
+        this.gameMenu.PrintTitle(); 
+
+        while (this.playerAmount != 1 || this.playerAmount != 2) { 
+            System.out.println("[Insertar mensaje de numero de jugadores]");
+            scanner.nextShort();
+            System.out.println("Los modos de juego disponibles son:\n 1: para un jugador\n 2: para dos jugadores\n\n");
+        }
+
+        
+    }
+
+    
+}
