9b8346ae0c005467b0bf097aef424f238f64cb40
Rapla913 <92961696+Rapla913@users.noreply.github.com>
2025-06-07 10:48:30 -0600

    Update Game.java

--- src/main/Game.java
+++ src/main/Game.java
@@ -5,6 +5,8 @@ public class Game {
     private Board gameBoard;
     private Scanner scanner; 
     private short playerAmount; 
+    private HumanPLayer playerOne, playerTwo; 
+    private IAPLayer sysPlayer; 
     /*
      * El constructor de la clase es el encargado crear los objetos que va a necesitar 
      * a lo largo del juego. 
@@ -17,21 +19,55 @@ public class Game {
         this.playerAmount = 0;  
     }
 
-    public void getWinner() {}
+    public void hasWon() {}
 
-    public void play(){
-        this.gameMenu.PrintTitle(); 
-
-        System.out.println("\n"); 
-         
+    public void setPlayerSymb(){  
         while (this.playerAmount != 1 || this.playerAmount != 2) { 
             System.out.println("ERROR"); 
-            System.out.println("Los modos de juego disponibles son:\n 1: para un jugador\n 2: para dos jugadores\n\n");    
+            System.out.println("""
+                               Los modos de juego disponibles son:
+                                    1: para un jugador
+                                    2: para dos jugadores
+                               """);    
             scanner.nextShort(); 
         }
-
-        
+        /*
+         * Hacer la asignación de los símbolos que usará cada jugador. 
+        */
+        if (playerAmount == 1) {
+            System.out.println("""
+                               Por favor seleccione su simbolo:
+                                    X 
+                                    O
+                               """);
+            this.playerOne = new HumanPlayer(scanner.nextLine());
+            this.sysPLayer = new IAPlayer(HumanPlayer.GetSymbol() == "O" ? "X" : "O");  
+        } else {
+            System.out.println("""
+                               Jugador 1: Por favor seleccione su simbolo:
+                                    X 
+                                    O
+                               """);
+            
+            this.playerOne = new HumanPlayer(scanner.nextLine());
+            this.sysPLayer = new IAPlayer(HumanPlayer.GetSymbol() == "O" ? "X" : "O");
+        }        
     }
 
+    public void play() {
+        this.gameMenu.PrintTitle(); 
+
+        setPlayerSymb(); 
+        System.out.println("Inicia el jugador 1"); 
+        /*
+         * Main game loop
+         */
+        while (true) {
+            /*
+             * Lógica del juego.
+             * El tablero no es accesible. La matriz es privada. 
+             */
+        }
+    }
     
 }
